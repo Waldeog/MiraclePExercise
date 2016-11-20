0f98f65d00cc0f666ad584d4027a362031a787c9
Valtteri Ranta <valtterih.ranta@gmail.com>
2016-11-20 19:15:48 +0200

    Added UIPickerView for selecting the State

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
-
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerButton: UIButton!
+    
+    let states = ["New York", "Alaska", "Arkansas", "California", "Maine", "Washington", "New Jersey"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        statePicker.dataSource = self
+        statePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +26,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func stateButtonPressed(_ sender: Any) {
+        statePicker.isHidden = false
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerButton.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+    }
 
 
 }
